750fa6da22db9ee45abd9807a5382cd277f89252
gialoc <nguyengialoc240101@gmail.com>
2025-10-01 14:37:19 +0700

    8. nav bar

---
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { WrapperContent, WrapperLabelText, WrapperTextPrice, WrapperTextValue } from './style'
+import { Checkbox, Col, Rate, Row } from 'antd'
+
+const NavbarComponent = () => {
+    const onChange = (checkedValues) => {
+        console.log('checked = ', checkedValues);
+    }
+    const renderContent = (type, options) => {
+        switch (type) {
+            case 'text':
+                return options.map((option, index) => {
+                    return <WrapperTextValue key={index}>{option}</WrapperTextValue>
+                })
+            case 'checkbox':
+                return <Checkbox.Group style={{ width: '100%', flexDirection: 'column', gap: '12px' }} onChange={onChange} >
+                    {options.map((option, index) => {
+                        return (
+                            <Checkbox style={{ marginLeft: '0px' }} value={option.value} key={index}>{option.label}</Checkbox>
+                        )
+                    })}
+                </Checkbox.Group>
+            case 'star':
+                return options.map((option, index) => {
+                    return (
+                        <div key={index} style={{ display: 'flex', alignItems: 'center' }}  >
+                            <Rate style={{ fontSize: '12px' }} key={index} disabled defaultValue={option} />
+                            <span>{`Tu ${option} sao`}</span>
+                        </div>
+
+                    )
+                })
+            case 'price':
+                return options.map((option, index) => {
+                    return (
+                        <WrapperTextPrice key={index} >
+                            <span>{option}</span>
+                        </WrapperTextPrice>
+
+                    )
+                })
+            default:
+                return {}
+        }
+    }
+    return (
+        <div >
+            <WrapperLabelText>Label</WrapperLabelText>
+            <WrapperContent>
+                {renderContent('text', ['Tu lanh', 'TV', 'May giat'])}
+                {renderContent('checkbox', [
+                    { label: 'A', value: 'A' },
+                    { label: 'B', value: 'B' },
+                    { label: 'C', value: 'C' },
+                ])}
+            </WrapperContent>
+            <WrapperContent>
+                {renderContent('price', ['dưới 40', 'lên 50.000'])}
+            </WrapperContent>
+        </div>
+    )
+}
+
+export default NavbarComponent
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,6 +6,7 @@ import slider1 from '../../assets/images/slide1.webp'
 import slider2 from '../../assets/images/slide2.webp'
 import slider3 from '../../assets/images/slide3.webp'
 import CardComponent from '../../components/CardComponent/CardComponent'
+import NavbarComponent from '../../components/NavbarComponent/NavbarComponent'
 
 const HomePage = () => {
   const arr = ['TV', 'Tu lanh', 'Laptop']
@@ -25,6 +26,7 @@ const HomePage = () => {
         <div style={{ marginTop: '20px', display: 'flex', alignItems: 'center', gap: '20px' }}>
           <CardComponent />
         </div>
+        <NavbarComponent />
       </div>
     </>
   )
